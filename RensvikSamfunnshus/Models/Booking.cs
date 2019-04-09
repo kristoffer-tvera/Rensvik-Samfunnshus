ebc2d6b4d7abc3ffedd058d87aa4870a4ff9e049
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-04-10 00:10:43 +0200

    Prepared the class for being a poco db table model

--- RensvikSamfunnshus/Models/Booking.cs
+++ RensvikSamfunnshus/Models/Booking.cs
@@ -1,18 +1,44 @@
 ﻿using System;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
 
 namespace RensvikSamfunnshus.Models
 {
+    [TableName("Booking")]
     public class Booking
     {
+        [Column("Id")]
+        [PrimaryKeyColumn(AutoIncrement = true)]
+        public int Id { get; set; }
+
+        [Column("Requested")]
         public DateTime Requested { get; set; }
+
+        [Column("From")]
         public DateTime From { get; set; }
+
+        [Column("To")]
         public DateTime To { get; set; }
+
+        [Column("Area")]
         public string Area { get; set; }
+
+        [Column("Telephone")]
         public string Telephone { get; set; }
+
+        [Column("Email")]
         public string Email { get; set; }
+
+        [Column("Comment")]
         public string Comment { get; set; }
+
+        [Column("Wash")]
         public bool Wash { get; set; }
+
+        [Column("Approved")]
         public bool Approved { get; set; }
+
+        [Column("Payment")]
         public DateTime? Payment { get; set; }
 
     }
