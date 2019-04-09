cbf9c07aa1ac586be016bfec6345ba7fb1b31fa1
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-04-10 00:11:52 +0200

    Umbraco Event Listener to add the table on boot if its not already up

---
+++ RensvikSamfunnshus/App_Start/Events.cs
@@ -0,0 +1,20 @@
+﻿using RensvikSamfunnshus.Models;
+using Umbraco.Core;
+using Umbraco.Core.Persistence;
+
+namespace RensvikSamfunnshus.App_Start
+{
+    public class Events : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var dbContext = applicationContext.DatabaseContext;
+            var db = new DatabaseSchemaHelper(dbContext.Database, applicationContext.ProfilingLogger.Logger, dbContext.SqlSyntax);
+
+            if (!db.TableExist<Booking>())
+            {
+                db.CreateTable<Booking>();
+            }
+        }
+    }
+}
