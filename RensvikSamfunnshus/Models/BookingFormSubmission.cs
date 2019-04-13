925d0d7e1fbd033ba3c755bb834dead19195bfe8
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-04-13 21:19:49 +0200

    Viewmodel and partial view for form submissions (frontend) to bookings

---
+++ RensvikSamfunnshus/Models/BookingFormSubmission.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RensvikSamfunnshus.Models
+{
+    public class BookingFormSubmission
+    {
+        [Required]
+        [DisplayName("Fra")]
+        public DateTime From { get; set; }
+
+        [Required]
+        [DisplayName("Til")]
+        public DateTime To { get; set; }
+
+        [Required]
+        [DisplayName("Område")]
+        public string Area { get; set; }
+
+        [Required]
+        [DisplayName("Telephone number")]
+        public string Telephone { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [DisplayName("Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Wash")]
+        public bool Wash { get; set; }
+    }
+}
