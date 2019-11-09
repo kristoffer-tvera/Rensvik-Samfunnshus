68cb756b841eb40aaa40753f483f405a00733ce8
Kristoffer Tverå <Bluekristoffer@gmail.com>
2019-11-09 21:36:58 +0100

    Implemented a random string generator in the RandomHelper

--- RSH/Utility/RandomHelper.cs
+++ RSH/Utility/RandomHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 
 namespace RSH.Utility
@@ -31,6 +32,41 @@ namespace RSH.Utility
             lock (syncLock)
             {
                 return random.Next(0, max);
+            }
+        }
+
+        public static string RandomString(int length, string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        {
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[length];
+                byte[] buffer = null;
+
+                var maxRandom = byte.MaxValue - ((byte.MaxValue + 1) % chars.Length);
+
+                crypto.GetBytes(data);
+
+                var result = new char[length];
+
+                for (int i = 0; i < length; i++)
+                {
+                    byte value = data[i];
+
+                    while (value > maxRandom)
+                    {
+                        if (buffer == null)
+                        {
+                            buffer = new byte[1];
+                        }
+
+                        crypto.GetBytes(buffer);
+                        value = buffer[0];
+                    }
+
+                    result[i] = chars[value % chars.Length];
+                }
+
+                return new string(result);
             }
         }
     }
